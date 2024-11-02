0fc3d04e47617d5cf8178c03fe6f53e34311e2aa
JosiasHenrique <joystickhenrii@gmail.com>
2024-11-02 15:20:22 -0300

    Função para pesquisar veículo por id concluído

--- src/main/java/com/mycompany/aluguelveiculos/Controller/ServicoController.java
+++ src/main/java/com/mycompany/aluguelveiculos/Controller/ServicoController.java
@@ -5,7 +5,11 @@ import com.mycompany.aluguelveiculos.Model.Carro;
 import com.mycompany.aluguelveiculos.Model.ListaServico;
 import com.mycompany.aluguelveiculos.Model.Moto;
 import com.mycompany.aluguelveiculos.Model.Servico;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 public class ServicoController {
 
@@ -29,5 +33,49 @@ public class ServicoController {
         }
 
         return ListaServico.getInstance().add(veiculo);
+    }
+
+    public List<Object> pesquisar(int id) {
+
+        boolean achou = false;
+        int posicao = -1;
+
+        for (int i = 0; i < ListaServico.getInstance().size(); i++) {
+
+            if (ListaServico.getInstance().get(i).getId() == id) {
+                achou = true;
+                posicao = i;
+            }
+        }
+
+        if (achou == true) {
+            Servico servico = ListaServico.getInstance().get(posicao);
+
+            int cnh = servico.getCnh();
+            String cliente = servico.getCliente();
+            String modeloVeiculo = servico.getModeloVeiculo();
+            Date dataRetirada = servico.getDataRetirada();
+            Date dataDevolucao = servico.getDataDevolucao();
+            boolean seguro = servico.isSeguro();
+            double valorAluguel = servico.getValorAluguel();
+
+            List<Object> dados = new ArrayList<>(Arrays.asList(id, cnh, cliente, modeloVeiculo, dataRetirada,
+                    dataDevolucao, seguro, valorAluguel));
+
+            if (servico instanceof Carro) {
+                Carro carro = (Carro) servico;
+                dados.add(carro.getQtdPassageiro());
+                dados.add(carro.isReboque());
+            } else if (servico instanceof Moto) {
+                Moto moto = (Moto) servico;
+                dados.add(moto.isItemDeSeguranca());
+                dados.add(moto.isItemDeArmazenamento());
+            }
+
+            return dados;
+        } else {
+            JOptionPane.showMessageDialog(null, "Veículo não encontrado!", "Aviso", 0);
+            return null;
+        }
     }
 }
